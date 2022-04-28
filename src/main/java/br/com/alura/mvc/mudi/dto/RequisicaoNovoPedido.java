41cd5c591094cca633dd33dbbf5fc344909bbed8
Ramon Nascimento <ramonboladao98@gmail.com>
2022-04-28 08:24:43 -0300

    fix / requisição 2

--- src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -4,11 +4,14 @@ import br.com.alura.mvc.mudi.model.Pedido;
 
 public class RequisicaoNovoPedido {
 	
-
+<<<<<<< aula3
 	private String nomeproduto;
 	private String urlProduto;
 	private String urlImagen;
 	private String descricao;
+<<<<<<< HEAD
+=======
+	
 	
 	public String getNomeproduto() {
 		return nomeproduto;
@@ -42,5 +45,13 @@ public class RequisicaoNovoPedido {
 		pedido.setUrlProduto(urlProduto);
 		return pedido;
 	}
+>>>>>>> branch 'aula3' of https://github.com/ramonptz/mudi-sys.git
 
+=======
+	private nomeproduto
+	urlProduto
+	urlImagen
+	descricao
+//
+>>>>>>> fb63e66 versão do pc-marinha desatualizado
 }
