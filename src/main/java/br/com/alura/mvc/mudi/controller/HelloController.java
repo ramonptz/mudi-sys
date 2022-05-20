08edd0550df075758eb3dd208e45bf37de0b31db
ramonptz <89748744+ramonptz@users.noreply.github.com>
2022-05-20 15:33:30 -0300

    tela de login
    adicionando a tela de login

--- src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -1,16 +1,15 @@
 package br.com.alura.mvc.mudi.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 	
-	@GetMapping("/hello")
-	public String hello(HttpServletRequest request) {
-		request.setAttribute("nome", "Mundo");
-		return "hello";
+	@GetMapping
+	@RequestMapping("/login")
+	String login() {
+		return "login";
 	}
 }
