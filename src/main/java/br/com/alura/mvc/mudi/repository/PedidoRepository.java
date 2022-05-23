40af0a71233af4c930c7ea3c31b1197e4de0dc9c
ramonptz <89748744+ramonptz@users.noreply.github.com>
2022-05-23 15:32:25 -0300

    pedidos listados de acordo com o usuario logado

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -3,6 +3,8 @@ package br.com.alura.mvc.mudi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
@@ -19,6 +21,12 @@ public interface PedidoRepository extends JpaRepository<Pedido, Long>{
 	public List<Pedido> findAll();
 
 	public List<Pedido> findByStatus(StatusPedido status);
+	
+	@Query("select p from Pedido p join p.user u where  u.username= :username")
+	
+	// select * from pedido inner join users on (pedido.user_username = users.username) where users.username= 'joao'; // sql convertido
+	public List<Pedido> findAllByUsuario(@Param("username") String username);
+
 	
 	
 	/* antigo pedido com springdata
